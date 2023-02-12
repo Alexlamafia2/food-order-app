de5aeaeb24482475b0b3637f6d969ab09ebbbd7e
Alexey Martinez <amartinez@Encore.local>
2023-02-12 09:53:42 -0500

    first commit

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,51 @@
+import React, { useContext } from "react";
+
+import Modal from "../UI/Modal";
+import CartContext from "../../store/cart-context";
+import styles from "./Cart.module.css";
+import CartItem from "./CartItem/CartItem";
+
+export default function Cart(props) {
+  const cartCtx = useContext(CartContext);
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
+
+  function cartItemRemoveHandler(id) {
+    cartCtx.removeItem(id);
+  }
+
+  function cartItemAddHandler(item) {
+    cartCtx.addItem({ ...item, amount: 1 });
+  }
+
+  const cartItems = (
+    <ul className={styles["cart-items"]}>
+      {cartCtx.items.map((item) => (
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
+        />
+      ))}
+    </ul>
+  );
+
+  return (
+    <Modal onClose={props.onClose}>
+      {cartItems}
+      <div className={styles.total}>
+        <span>Total Amount</span>
+        <span>{totalAmount}</span>
+      </div>
+      <div className={styles.actions}>
+        <button className={styles["button--alt"]} onClick={props.onClose}>
+          Close
+        </button>
+        {hasItems && <button className={styles.button}>Order</button>}
+      </div>
+    </Modal>
+  );
+}
---
+++ src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,42 @@
+import React, { useContext, useEffect, useState } from "react";
+
+import CartIcon from "../Cart/CartIcon";
+import CartContext from "../../store/cart-context";
+import styles from "./HeaderCartButton.module.css";
+
+export default function HeaderCartButton(props) {
+  const [btnHighlighted, setBtnHighlighted] = useState(false);
+  const cartCtx = useContext(CartContext);
+
+  const {items} = cartCtx;
+
+  const numberOfCartItems = items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
+
+  const btnStyles = `${styles.button} ${btnHighlighted ? styles.bump : '' }`;
+
+  useEffect(() =>{
+    if(items.length === 0){
+      return;
+    }
+    setBtnHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnHighlighted(false);
+    }, 300)
+    return () => {
+      clearTimeout(timer);
+    };
+  },[items]);
+
+  return (
+    <button className={btnStyles} onClick={props.onClick}>
+      <span className={styles.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={styles.badge}>{numberOfCartItems}</span>
+    </button>
+  );
+}
